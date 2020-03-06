2cf8e4a1809aa969df8e69af1064fc9405609896
happysmile911003 <happysmile911003@gmail.com>
2020-03-06 08:43:38 +0530

    implemented navbar without cartdropdown

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-
-
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import MainNavbar from "./components/navbar/navbar-component";
 
 function App() {
   return (
     <div className="App">
-      
+      <MainNavbar />
+      <div className="container"></div>
     </div>
   );
 }
---
+++ src/components/navbar/navbar-component.jsx
@@ -0,0 +1,64 @@
+import React, { Component, useState } from "react";
+import { Navbar, Nav, Button } from "react-bootstrap";
+import "./navbar-style.scss";
+
+const MainNavbar = props => {
+  const [logingStatus, setLogingStatus] = useState({
+    loggedin: true,
+    hidden: false
+  });
+
+  const signOut = () => {};
+
+  return (
+    <div className="navbar-head">
+      <Navbar
+        collapseOnSelect
+        expand="md"
+        bg="dark"
+        variant="dark"
+        style={{ width: "100%", backgroundColor: "dark" }}
+      >
+        <Navbar.Brand href="#home">Logo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#features">Shop</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          <Nav className=" float-right">
+            <Nav.Link href="#deets">ContactUs</Nav.Link>
+            <Nav.Link href="#deets">About</Nav.Link>
+            {logingStatus.loggedin ? (
+              <div className="signoutHeader" onClick={() => signOut()}>
+                <Nav.Link href="#deets">
+                  {" "}
+                  <Button variant="outline-success" style={{ padding: "3px" }}>
+                    Logout
+                  </Button>
+                </Nav.Link>
+              </div>
+            ) : (
+              <Nav>
+                <Nav.Link href="#deets">
+                  <Button variant="outline-success" style={{ padding: "3px" }}>
+                    Signin
+                  </Button>
+                </Nav.Link>
+                <Nav.Link href="#deets">
+                  <Button variant="outline-success" style={{ padding: "3px" }}>
+                    Login
+                  </Button>
+                </Nav.Link>
+              </Nav>
+            )}
+
+            {/* {logingStatus.hidden ? null : <CartDropdown />} */}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default MainNavbar;
