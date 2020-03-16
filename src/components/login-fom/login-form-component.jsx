fd6d29131facf9bf8671c2bb9bca446a93b09eb3
happysmile911003 <happysmile911003@gmail.com>
2020-03-16 10:17:35 +0530

    Implemented login component and add from validation using formic and yup and added relevent styles

---
+++ src/components/login-fom/login-form-component.jsx
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import { Formik } from "formik";
+import { Form, Col, InputGroup, Button } from "react-bootstrap";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .email()
+    .required("Enter the email"),
+  password: yup
+    .string()
+    .min(3, "min")
+    .required("Enter the password")
+});
+
+const LoginForm = props => {
+  const [loginData, setloginData] = useState({
+    email: "",
+    password: ""
+  });
+
+  return (
+    <div className="loginComponentHead">
+      <Formik
+        validationSchema={schema}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            console.log(values);
+            setloginData(values);
+          }, 500);
+        }}
+        initialValues={{
+          email: "",
+          password: ""
+        }}
+      >
+        {({
+          handleSubmit,
+          isSubmitting,
+          handleChange,
+          handleBlur,
+          values,
+          touched,
+          isValid,
+          errors
+        }) => (
+          <Form noValidate onSubmit={handleSubmit}>
+            <Form.Group as={Col} md="12" controlId="validationFormik01">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Email"
+                name="email"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                isInvalid={touched.email && errors.email}
+                isValid={touched.email && !errors.email}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.email}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md="12" controlId="validationFormik02">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                placeholder="Password"
+                type="password"
+                name="password"
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                isInvalid={touched.password && errors.password}
+                isValid={touched.password && !errors.password}
+              />
+
+              <Form.Control.Feedback type="invalid">
+                {errors.password}
+              </Form.Control.Feedback>
+            </Form.Group>
+
+            <Button
+              type="submit"
+              style={{ marginLeft: "13px" }}
+              disabled={isSubmitting}
+            >
+              Submit form
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default LoginForm;
--- src/pages/login-signup/login-signup-component.js
+++ src/pages/login-signup/login-signup-component.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
+import LoginForm from "../../components/login-fom/login-form-component";
+
+import "./login-signup-styles.scss";
 
 const LoginSignup = props => {
   return (
-    <div className="loginSignupMainHead">
+    <div className="loginSignupMainHead container">
       <Row>
-        <Col md="6">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt
-          voluptates optio modi similique harum maiores quas minus ipsam, iusto
-          natus eum facere quisquam, rerum expedita illo nulla. Mollitia,
-          necessitatibus accusamus.
+        <Col md="5">
+          <div className="loginSignupPageLginForm">
+            <h4>I already have an account </h4>
+            <p>Signin using email and password</p>
+            <LoginForm />
+          </div>
         </Col>
-        <Col md="6">
+        <Col md="7">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt
           voluptates optio modi similique harum maiores quas minus ipsam, iusto
           natus eum facere quisquam, rerum expedita illo nulla. Mollitia,
