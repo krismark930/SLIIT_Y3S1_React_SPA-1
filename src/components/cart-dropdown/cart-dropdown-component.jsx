75ace4ada792aae2068b8ad357b149a76e0fa5df
happysmile911003 <happysmile911003@gmail.com>
2020-03-16 15:57:48 +0530

    completed cart dropdown item component

--- src/components/cart-dropdown/cart-dropdown-component.jsx
+++ src/components/cart-dropdown/cart-dropdown-component.jsx
@@ -5,17 +5,41 @@ import { Col, Button } from "react-bootstrap";
 import "./cart-dropdown-styles.scss";
 import CartItem from "../cart-item/cart-item-component";
 
-const CartDropdown = ({ cartItems, history, dispatch }) => {
+const cartItems = [
+  {
+    id: 1,
+    name: "Brown Brim",
+    imageUrl: "https://i.ibb.co/ZYW3VTp/brown-brim.png",
+    price: 25,
+    quantity: 3
+  },
+  {
+    id: 1,
+    name: "Brown Brim",
+    imageUrl: "https://i.ibb.co/ZYW3VTp/brown-brim.png",
+    price: 25,
+    quantity: 3
+  },
+  {
+    id: 1,
+    name: "Brown Brim",
+    imageUrl: "https://i.ibb.co/ZYW3VTp/brown-brim.png",
+    price: 25,
+    quantity: 3
+  }
+];
+
+const CartDropdown = ({ history, dispatch }) => {
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
-        {/* {cartItems.length ? (
+        {cartItems.length ? (
           cartItems.map(cartItem => (
             <CartItem key={cartItem.id} item={cartItem} />
           ))
         ) : (
           <span className="red-text empty-cart">Cart is Empty</span>
-        )} */}
+        )}
       </div>
 
       <Button
