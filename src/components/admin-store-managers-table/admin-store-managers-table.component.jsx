227d63c515990523088277ad329ee8594636d4f1
happysmile911003 <happysmile911003@gmail.com>
2020-05-16 12:58:01 +0530

    minor changes

--- src/components/admin-store-managers-table/admin-store-managers-table.component.jsx
+++ src/components/admin-store-managers-table/admin-store-managers-table.component.jsx
@@ -1,82 +1,63 @@
-import React, {useContext, useEffect, useState} from "react";
-
-import "./admin-store-managers-table-styles.scss";
-//import CheckoutItem from "../../components/checkout-item/checkout-item-component";
-import {AppContext} from "../../Context/app-context";
-import axios from "axios";
+import React, {useContext, useEffect, useState} from 'react'
+import './admin-store-managers-table-styles.scss'
+import {AppContext} from '../../Context/app-context'
+import axios from 'axios'
 
 const ManageStoreManagerTable = () => {
-  const appContext = useContext(AppContext);
-  const [loading, setLoading] = useState(false);
-  const [isDelete, setIsDelete] = useState(false);
-  const [storeManagers, setStoreManagers] = useState([]);
+  const appContext = useContext(AppContext)
+  const [loading, setLoading] = useState(false)
+  const [isDelete, setIsDelete] = useState(false)
+  const [storeManagers, setStoreManagers] = useState([])
 
   const getStoreManagers = async () => {
-    console.log("hi details ganna awa");
     try {
-      const response = await fetch("http://localhost:5000/admin/storemanager");
-
-      const responseData = await response.json();
-      //const userid= appContext.editPayUserId;
-      setStoreManagers(responseData);
-      appContext.addStoreManagers(responseData);
-      console.log(responseData);
-
-    } catch (errorss) {
-      console.log(errorss);
+      const response = await fetch('http://localhost:5000/admin/storemanager')
+      const responseData = await response.json()
+      setStoreManagers(responseData)
+      appContext.addStoreManagers(responseData)
+    } catch (errors) {
+      console.log(errors)
     }
-
   }
+
   useEffect(() => {
-    getStoreManagers();
-  }, [getStoreManagers, storeManagers]);
+    getStoreManagers()
+  }, [getStoreManagers, storeManagers])
 
   const EditStoreManager = (id) => {
-    appContext.storeManagerEdit();
-    appContext.setEditStoreManagerId(id);
-
+    appContext.storeManagerEdit()
+    appContext.setEditStoreManagerId(id)
     axios.get('http://localhost:5000/admin/storemanager/' + id)
       .then(response => {
-
-        //appContext.addStoreManagers(response.data);
-        console.log(response.data)
+        appContext.addStoreManagers(response.data)
       })
       .catch(function (error) {
-        console.log(error);
+        console.log(error)
       })
   }
 
   const DeleteStoreManager = async (id) => {
     try {
-
       const response = await fetch('http://localhost:5000/admin/storemanager/' + id, {
-        method: "DELETE",
+        method: 'DELETE',
         headers: {
-          "Content-Type": "application/json"
+          'Content-Type': 'application/json'
         },
-      });
-
-      const responseData = await response.json();
-      console.log(responseData);
-      //console.log("hi delete una");
-      // currentEmail = " ";
-      //getPayUserDetails();
-      if (responseData) {
-        setIsDelete(true);
-      }
-      setLoading(false);
-
-    } catch (errorss) {
-      console.log(errorss);
-      setLoading(false);
-
+      })
+      const responseData = await response.json()
+      if (responseData)
+        setIsDelete(true)
+      setLoading(false)
+    } catch (errors) {
+      setLoading(false)
+      console.log(errors)
     }
   }
 
   return (
     <div>
-      <table className="table">
-        <thead className="thead-light">
+      <table className='table'>
+        <thead className='thead-light'>
         <tr>
           <th>First Name</th>
           <th>Last Name</th>
@@ -88,7 +69,6 @@ const ManageStoreManagerTable = () => {
           <th>Type</th>
         </tr>
         </thead>
-
         <tbody>
         {storeManagers.map((storeManager) => {
           return (
@@ -106,13 +86,12 @@ const ManageStoreManagerTable = () => {
                 <button onClick={() => EditStoreManager(storeManager._id)}>Edit</button>
               </td>
             </tr>
-
           )
         })}
         </tbody>
       </table>
     </div>
-  );
-};
+  )
+}
 
-export default ManageStoreManagerTable;
+export default ManageStoreManagerTable
