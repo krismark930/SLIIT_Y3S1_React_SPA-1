659933a9323ba71b633b138c2a217860fb925b1b
happysmile911003 <happysmile911003@gmail.com>
2020-05-18 16:45:21 +0530

    Used home-category-component in Homepage

--- src/components/home-item/home-item-component.jsx
+++ src/components/home-item/home-item-component.jsx
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Card, Button } from "react-bootstrap";
 import { FaCaretDown } from "react-icons/fa";
+import { AppContext } from "../../Context/app-context";
 
-const HomeItem = ({ productName, price, productImage }) => {
+const HomeItem = ({ item }) => {
+  const appContext = useContext(AppContext);
+  const urls = item.productImage;
   return (
     <Card className="col-3">
-      <Card.Img variant="top" src={require({ productImage })} />
+      <Card.Img variant="top" src={require("../../assets/img1.jpg")} />
       <Card.Body>
-        <Card.Title style={{ textAlign: "center" }}>{productName}</Card.Title>
-        <Card.Text style={{ textAlign: "center" }}>{price}</Card.Text>
+        <Card.Title style={{ textAlign: "center" }}>
+          {item.productName}
+        </Card.Title>
+        <Card.Text style={{ textAlign: "center" }}>{item.price}</Card.Text>
       </Card.Body>
       <Card.Footer style={{ textAlign: "center" }}>
         <Button
+          onClick={() => appContext.addItemToCart(item)}
           variant="dark"
           style={{
             margin: "0px auto",
-            width: "60%",
+            width: "80%",
           }}
         >
           Add to Cart{" "}
--- src/pages/homepage/homepage-component.jsx
+++ src/pages/homepage/homepage-component.jsx
@@ -1,20 +1,92 @@
-import React, {useContext} from "react";
-import {AppContext} from "../../Context/app-context";
-import {Button} from "react-bootstrap";
+import React, { useContext } from "react";
+import { AppContext } from "../../Context/app-context";
+import { Button } from "react-bootstrap";
+import HomeCategory from "../../components/home-category/home-category-component";
+import { wait } from "@testing-library/react";
 
 const Homepage = () => {
   const appContext = useContext(AppContext);
+  const categories = [
+    {
+      categoryTitle: "Hats",
+    },
+    {
+      categoryTitle: "Men",
+    },
+    {
+      categoryTitle: "Women",
+    },
+    {
+      categoryTitle: "Shoes",
+    },
+  ];
+
+  const products = [
+    {
+      title: "Women",
+      price: 123.0,
+      category: "Women",
+      productImage: "https://i.ibb.co/ZYW3VTp/brown-brim.png",
+    },
+    {
+      title: "Men",
+      price: 123.0,
+      category: "Men",
+      productImage: "https://i.ibb.co/ZYW3VTp/brown-brim.png",
+    },
+    {
+      title: "Men",
+      price: 123.0,
+      category: "Men",
+      productImage: "https://i.ibb.co/ZYW3VTp/brown-brim.png",
+    },
+    {
+      title: "Shoes",
+      price: 123.0,
+      category: "Shoes",
+      productImage: "https://i.ibb.co/ZYW3VTp/brown-brim.png",
+    },
+    {
+      title: "Women",
+      price: 123.0,
+      category: "Women",
+      productImage: "https://i.ibb.co/ZYW3VTp/brown-brim.png",
+    },
+    {
+      title: "Produt1",
+      price: 123.0,
+      category: "Women",
+      productImage: "https://i.ibb.co/ZYW3VTp/brown-brim.png",
+    },
+    {
+      title: "Produt1",
+      price: 123.0,
+      category: "Women",
+      productImage: "https://i.ibb.co/ZYW3VTp/brown-brim.png",
+    },
+    {
+      title: "Produt1",
+      price: 123.0,
+      category: "Hats",
+      productImage: "https://i.ibb.co/ZYW3VTp/brown-brim.png",
+    },
+  ];
+
+  const chats = categories.map((item) => {
+    return <HomeCategory category={item.categoryTitle} products={products} />;
+  });
 
   return (
-    <div className="homepageHead">
-      {appContext.products.map(cartItem => (
+    <div className="homepageHead" style={{ marginTop: "5%" }}>
+      {/* {appContext.products.map(cartItem => (
         <div key={cartItem.id}>
           <p>{cartItem.id}</p>
           <Button onClick={() => appContext.addItemToCart(cartItem)}>
             Add to cart
           </Button>
         </div>
-      ))}
+      ))} */}
+      {chats}
     </div>
   );
 };
