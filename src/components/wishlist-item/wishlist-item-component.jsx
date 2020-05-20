0e3dfe067ae0d90e241cccb4e1460e81f93e423b
happysmile911003 <happysmile911003@gmail.com>
2020-05-20 13:51:40 +0530

    Implemented wishlist item component

--- src/components/wishlist-item/wishlist-item-component.jsx
+++ src/components/wishlist-item/wishlist-item-component.jsx
@@ -1,26 +1,26 @@
-// import React, { useContext } from "react";
-// import { AppContext } from "../../Context/app-context";
-// import "./wishlist-item-styles.scss";
+import React, { useContext } from "react";
+import { AppContext } from "../../Context/app-context";
+import "./wishlist-item-styles.scss";
 
-// const WishListItem = ({ product }) => {
-//   const appContext = useContext(AppContext);
+const WishListItem = ({ product }) => {
+  const appContext = useContext(AppContext);
 
-//   return (
-//     <div className="wishlist-item" style={{ alignItems: "center" }}>
-//       <div className="image-container">
-//         <img src={product.imageUrl} alt="item" />
-//       </div>
-//       <span className="name">{product.title}</span>
+  return (
+    <div className="wishlist-item" style={{ alignItems: "center" }}>
+      <div className="image-container">
+        <img src={product.imageUrl} alt="item" />
+      </div>
+      <span className="name">{product.title}</span>
 
-//       <span className="price">{product.price}</span>
-//       <div
-//         className="remove-button"
-//         // onClick={() => appContext.removeCompletelyItemFromCart(productID)}
-//       >
-//         &#10005;
-//       </div>
-//     </div>
-//   );
-// };
+      <span className="price">{product.price}</span>
+      <div
+        className="remove-button"
+        // onClick={() => appContext.removeCompletelyItemFromCart(productID)}
+      >
+        &#10005;
+      </div>
+    </div>
+  );
+};
 
-// export default WishListItem;
+export default WishListItem;
