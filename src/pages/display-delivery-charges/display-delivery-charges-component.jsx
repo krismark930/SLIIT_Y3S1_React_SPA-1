7212b12e9241e3c0ea8fa290acc6368a666add1a
happysmile911003 <happysmile911003@gmail.com>
2020-05-21 14:57:51 +0530

    added some things to delivery charges table

--- src/pages/display-delivery-charges/display-delivery-charges-component.jsx
+++ src/pages/display-delivery-charges/display-delivery-charges-component.jsx
@@ -10,7 +10,7 @@ const DisplayDeliveryCharges = (props) => {
   return (
 
     <div>
-      <table className="table">
+      <table className="table" style={{float: "center"}}>
         <thead className="thead-light">
         <tr>
           <th>Province</th>
@@ -19,39 +19,39 @@ const DisplayDeliveryCharges = (props) => {
         </thead>
         <tbody>
         <tr>
-          <td style={{textAlign: "center"}}>1.Western</td>
+          <td style={{textAlign: "left"}}>1.Western</td>
           <td>$100</td>
         </tr>
         <tr>
-          <td style={{textAlign: "center"}}>2.Eastern</td>
+          <td style={{textAlign: "left"}}>2.Eastern</td>
           <td>$200</td>
         </tr>
         <tr>
-          <td style={{textAlign: "center"}}>3.North Central</td>
+          <td style={{textAlign: "left"}}>3.North Central</td>
           <td>$300</td>
         </tr>
         <tr>
-          <td style={{textAlign: "center"}}>4.Northern</td>
+          <td style={{textAlign: "left"}}>4.Northern</td>
           <td>$400</td>
         </tr>
         <tr>
-          <td style={{textAlign: "center"}}>5.North Western</td>
+          <td style={{textAlign: "left"}}>5.North Western</td>
           <td>$500</td>
         </tr>
         <tr>
-          <td style={{textAlign: "center"}}>6.Sabaragamuwa</td>
+          <td style={{textAlign: "left"}}>6.Sabaragamuwa</td>
           <td>$600</td>
         </tr>
         <tr>
-          <td style={{textAlign: "center"}}>7.Southern</td>
+          <td style={{textAlign: "left"}}>7.Southern</td>
           <td>$700</td>
         </tr>
         <tr>
-          <td style={{textAlign: "center"}}>8.Uva</td>
+          <td style={{textAlign: "left"}}>8.Uva</td>
           <td>$800</td>
         </tr>
         <tr>
-          <td style={{textAlign: "center"}}>9.Central</td>
+          <td style={{textAlign: "left"}}>9.Central</td>
           <td>$900</td>
         </tr>
         </tbody>
