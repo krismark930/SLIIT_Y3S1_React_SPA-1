baa99ec7d2ab2bdb9e0b3be98cb221b59b6ba1d0
happysmile911003 <happysmile911003@gmail.com>
2020-05-22 17:50:52 +0530

    Added heart buttons to homeitem component

--- src/components/home-item/home-item-component.jsx
+++ src/components/home-item/home-item-component.jsx
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 import { Button, Card } from "react-bootstrap";
 import { FaCartArrowDown } from "react-icons/fa";
 import { AppContext } from "../../Context/app-context";
+import { FaRegHeart } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
 
 const HomeItem = ({ item }) => {
   const appContext = useContext(AppContext);
@@ -19,8 +21,38 @@ const HomeItem = ({ item }) => {
         src={require("../../assets/img1.jpg")}
       />
 
-      <Card.Title style={{ textAlign: "center", margin: "10px" }}>
-        {titlePrice}
+      <Card.Title
+        style={{
+          textAlign: "center",
+          margin: "10px",
+          display: "flex",
+          width: "100%",
+          justifyContent: "space-between",
+        }}
+      >
+        <span>{item.title}</span>
+        <span>`$${item.price}`</span>
+        {item.wishList ? (
+          <span>
+            <FaHeart
+              style={{
+                marginRight: "6px",
+                marginLeft: "6px",
+                marginBottom: "3px",
+              }}
+            />
+          </span>
+        ) : (
+          <span>
+            <FaRegHeart
+              style={{
+                marginRight: "6px",
+                marginLeft: "6px",
+                marginBottom: "3px",
+              }}
+            />
+          </span>
+        )}
       </Card.Title>
 
       <Card.Footer style={{ textAlign: "center", backgroundColor: "white" }}>
