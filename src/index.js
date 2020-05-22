bdad4fb5a5ce5063990aa62871e2b2f99618f9b7
happysmile911003 <happysmile911003@gmail.com>
2020-05-22 21:22:44 +0530

    ui changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import GlobalState from "./Context/global-state";
 import WebFont from "webfontloader";
 
@@ -16,7 +16,7 @@ WebFont.load({
 ReactDOM.render(
   <BrowserRouter>
     <GlobalState>
-      <App />
+      <App/>
     </GlobalState>
   </BrowserRouter>,
   document.getElementById("root")
