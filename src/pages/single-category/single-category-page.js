41dfc70154e69ff28ebf1832b4afd76b71d95613
happysmile911003 <happysmile911003@gmail.com>
2020-05-23 01:19:51 +0530

    Finished styling single category page

--- src/pages/single-category/single-category-page.js
+++ src/pages/single-category/single-category-page.js
@@ -1,6 +1,7 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import HomeItem from "../../components/home-item/home-item-component";
-import {AppContext} from "../../Context/app-context";
+import { AppContext } from "../../Context/app-context";
+import "./single-category-styles.scss";
 
 const SingleCategory = (props) => {
   const appContext = useContext(AppContext);
@@ -11,16 +12,29 @@ const SingleCategory = (props) => {
   );
 
   return (
-    <div className="home-category-wrapper container cardDeckHome">
-      <h3 style={{padding: "10px", marginLeft: "10px ", marginTop: "3%"}}>
-        {props.match.params.category}
-      </h3>
-      <div className="row">
-        <div className="col">
-          <div className="row">
-            {filtered.map((item) => {
-              return <HomeItem item={item}/>;
-            })}
+    <div className="singleCategoryTop">
+      <div className="home-category-wrapper container cardDeckHome">
+        <h3
+          style={{
+            padding: "10px",
+            marginLeft: "10px ",
+            fontSize: "35px",
+            marginTop: "3%",
+            fontFamily: "Roboto Slab",
+            fontWeight: "700",
+            textAlign: "center",
+            marginBottom: "20px",
+          }}
+        >
+          {props.match.params.category}
+        </h3>
+        <div className="row">
+          <div className="col">
+            <div className="row">
+              {filtered.map((item) => {
+                return <HomeItem item={item} />;
+              })}
+            </div>
           </div>
         </div>
       </div>
