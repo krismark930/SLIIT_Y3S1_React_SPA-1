4be99382192ab5db3c32a9e3dc25038fce8ba720
happysmile911003 <happysmile911003@gmail.com>
2020-05-23 22:49:04 +0530

    Resolve merger conflicts

--- src/pages/single-product/single-product.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SingleProductView = (props) => {
-  console.log(props.match.params.product);
-
-  return <div className="singleproductHeader"></div>;
-};
-
-export default SingleProductView;
