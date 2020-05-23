b464041fc630f16703078a2aed2a4beb5d8ffd2b
happysmile911003 <happysmile911003@gmail.com>
2020-05-23 17:34:46 +0530

    implemented getting products from db and set them to context

--- src/Context/app-context.js
+++ src/Context/app-context.js
@@ -1,4 +1,4 @@
-import {createContext} from 'react'
+import { createContext } from "react";
 
 export const AppContext = createContext({
   checkCustomer: false,
@@ -12,13 +12,13 @@ export const AppContext = createContext({
   tempProducts: [],
   hidden: false,
   wishList: null,
-  editPayUserId: '1',
-  editPayCardId: '1',
+  editPayUserId: "1",
+  editPayCardId: "1",
   products: [
-    {id: 'p1', title: 'Gaming Mouse', price: 29.99},
-    {id: 'p2', title: 'Harry Potter 3', price: 9.99},
-    {id: 'p3', title: 'Used plastic bottle', price: 0.99},
-    {id: 'p4', title: 'Half-dried plant', price: 2.99}
+    { id: "p1", title: "Gaming Mouse", price: 29.99 },
+    { id: "p2", title: "Harry Potter 3", price: 9.99 },
+    { id: "p3", title: "Used plastic bottle", price: 0.99 },
+    { id: "p4", title: "Half-dried plant", price: 2.99 },
   ],
   cart: [],
   currentUser: [],
@@ -30,100 +30,57 @@ export const AppContext = createContext({
   payOrderDetails: [],
   editPayCardDetails: [],
   editStoreManager: false,
-  editStoreManagerId: '1',
+  editStoreManagerId: "1",
   storeManagers: [],
   editCategory: false,
-  editCategoryId: '1',
+  editCategoryId: "1",
   categories: [],
   existingCategory: false,
   existingStoreManager: false,
 
-  addItemToCart: (item) => {
-  },
-  removeItemFromCart: (productId, state) => {
-  },
-  removeCompletelyItemFromCart: (item) => {
-  },
-  toggleDropdownHidden: (state) => {
-  },
-  logout: (state) => {
-  },
-  login: (state) => {
-  },
-  setTruePayUserConfirmed: (state) => {
-  },
-  setFalsePayUserConfirmed: (state) => {
-  },
-  setTruePayCardConfirmed: (state) => {
-  },
-  setFalsePayCardConfirmed: (state) => {
-  },
-  addCurrentUser: (user) => {
-  },
-  addCurrentUserFirstName: (name) => {
-  },
-  addCurrentUserLastName: (name) => {
-  },
-  addPayUserDetails: (payUser) => {
-  },
-  addPayCardDetails: (payUser) => {
-  },
-  addPayOrderDetails: (payUser) => {
-  },
-  addEditPayUserDetails: (payUser) => {
-  },
-  addEditPayCardDetails: (payCard) => {
-  },
-  payUserEdit: (state) => {
-  },
-  payUserEditFalse: (state) => {
-  },
-  setEditPayUserID: (id) => {
-  },
-  payCardEdit: (state) => {
-  },
-  payCardEditFalse: (state) => {
-  },
-  setEditPayCardID: (id) => {
-  },
-  setWishListmethod: (mail) => {
-  },
-  addToWishList: (product) => {
-  },
-  setWishListMethod: () => {
-  },
-  getWishList: () => {
-  },
-  setChangeWishListProduct: (p) => {
-  },
-  setCheckAdminMethod: () => {
-  },
-  setCheckCustomerMethod: () => {
-  },
-  setChecksetCheckStoreManagerMethod: () => {
-  },
-  storeManagerEdit: () => {
-  },
-  editStoreManagerFalse: () => {
-  },
-  setEditStoreManagerId: () => {
-  },
-  addStoreManagers: () => {
-  },
-  categoryEdit: () => {
-  },
-  editCategoryFalse: () => {
-  },
-  setEditCategoryId: () => {
-  },
-  addCategories: () => {
-  },
-  existingCategoryEdit: () => {
-  },
-  editExistingCategoryFalse: () => {
-  },
-  existingStoreManagerEdit: () => {
-  },
-  editExistingStoreManagerFalse: () => {
-  }
-})
+  addItemToCart: (item) => {},
+  removeItemFromCart: (productId, state) => {},
+  removeCompletelyItemFromCart: (item) => {},
+  toggleDropdownHidden: (state) => {},
+  logout: (state) => {},
+  login: (state) => {},
+  setTruePayUserConfirmed: (state) => {},
+  setFalsePayUserConfirmed: (state) => {},
+  setTruePayCardConfirmed: (state) => {},
+  setFalsePayCardConfirmed: (state) => {},
+  addCurrentUser: (user) => {},
+  addCurrentUserFirstName: (name) => {},
+  addCurrentUserLastName: (name) => {},
+  addPayUserDetails: (payUser) => {},
+  addPayCardDetails: (payUser) => {},
+  addPayOrderDetails: (payUser) => {},
+  addEditPayUserDetails: (payUser) => {},
+  addEditPayCardDetails: (payCard) => {},
+  payUserEdit: (state) => {},
+  payUserEditFalse: (state) => {},
+  setEditPayUserID: (id) => {},
+  payCardEdit: (state) => {},
+  payCardEditFalse: (state) => {},
+  setEditPayCardID: (id) => {},
+  setWishListmethod: (mail) => {},
+  addToWishList: (product) => {},
+  setWishListMethod: () => {},
+  getWishList: () => {},
+  setChangeWishListProduct: (p) => {},
+  setCheckAdminMethod: () => {},
+  setCheckCustomerMethod: () => {},
+  setChecksetCheckStoreManagerMethod: () => {},
+  storeManagerEdit: () => {},
+  editStoreManagerFalse: () => {},
+  setEditStoreManagerId: () => {},
+  addStoreManagers: () => {},
+  categoryEdit: () => {},
+  editCategoryFalse: () => {},
+  setEditCategoryId: () => {},
+  addCategories: () => {},
+  existingCategoryEdit: () => {},
+  editExistingCategoryFalse: () => {},
+  existingStoreManagerEdit: () => {},
+  editExistingStoreManagerFalse: () => {},
+  setProductsMethod: () => {},
+});
